76095dc43ec05105a66e2b7434c6a4078549a793
liu946 <liuyang570926881@gmail.com>
2018-05-21 16:09:22 +0800

    read encode check

--- structure/data_iter.py
+++ structure/data_iter.py
@@ -7,15 +7,18 @@ import codecs
 def conll_sentence_iter(filename):
     with codecs.open(filename, encoding='utf8') as fi:
         lines = []
-        for i in fi:
-            i = i.strip()
-            if (i == '' or i.startswith('#')) and len(lines):
+        try:
+            for line_num, i in enumerate(fi):
+                i = i.strip()
+                if (i == '' or i.startswith('#')) and len(lines):
+                    yield lines
+                    lines = []
+                elif not (i == '' or i.startswith('#')):
+                    lines.append(i.split('\t'))
+            if len(lines):
                 yield lines
-                lines = []
-            elif not (i == '' or i.startswith('#')):
-                lines.append(i.split('\t'))
-        if len(lines):
-            yield lines
+        except UnicodeDecodeError as e:
+            raise UnicodeDecodeError(str(e) + ('line:%d [%s]' % (line_num, i)))
 
 
 def ws_sentence_iter(filename):
